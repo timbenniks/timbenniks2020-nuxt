da4ed516370deadd0f4709ae90f0d5e45ce8323b
Tim Benniks <tbenniks@gmail.com>
2021-02-09 12:11:47 +0100

    dependabot issues + extra analytics

--- plugins/analytics.js
+++ plugins/analytics.js
@@ -10,6 +10,12 @@ function send(data) {
         tid: 'UA-6797812-3',
         cid: `${Date.now()}${Math.random()}`,
         dl: location.href,
+        ua: navigator.userAgent,
+        dr: document.referrer || '',
+        sr: `${screen.width}x${screen.height}`,
+        vp: `${document.documentElement.clientWidth}x${document.documentElement.clientHeight}`,
+        sd: `${screen.pixelDepth}-bits`,
+        ul: navigator.language,
       },
       ...data,
     }).toString()
