f329b533d55f25799374e86b0c6c040fd41df7a6
Tim Benniks <tbenniks@gmail.com>
2021-05-02 23:04:13 +0200

    removed startpage stuff

--- store/store.js
+++ store/store.js
@@ -1,60 +1,6 @@
 import { asDay, asMonth, asYear } from '@/datalayer/helpers/modifiers'
 
 export const state = () => ({
-  youTubeStats: {
-    subscriberCount: 0,
-    viewCount: 0,
-    videoCount: 0,
-  },
-  twitterStats: {
-    tweets: {
-      amount: 0,
-      direction: null,
-      trend: 0,
-    },
-    impressions: {
-      amount: 0,
-      direction: null,
-      trend: 0,
-    },
-    profileVisits: {
-      amount: 0,
-      direction: null,
-      trend: 0,
-    },
-    mentions: {
-      amount: 0,
-      direction: null,
-      trend: 0,
-    },
-    followers: {
-      amount: 0,
-      direction: null,
-      trend: 0,
-    },
-  },
-  sponsorStats: {
-    supporters: [],
-    subscriptions: [],
-    sponsorCount: 0,
-    subscriberCount: 0,
-    supporterCount: 0,
-    supporterEarnings: 0,
-  },
-  plausibleStats: {
-    bounce_rate: {
-      value: 0,
-      change: 0,
-    },
-    pageviews: {
-      value: 0,
-      change: 0,
-    },
-    visitors: {
-      value: 0,
-      change: 0,
-    },
-  },
   searchResults: {
     query: '',
     prismicTags: [],
@@ -66,43 +12,6 @@ export const state = () => ({
 })
 
 export const mutations = {
-  setYouTubeStats(state, stats) {
-    state.youTubeStats = stats.channelStats
-  },
-
-  setTwitterStats(state, stats) {
-    state.twitterStats = {
-      ...stats.summary,
-    }
-  },
-
-  setPlausibleStats(state, stats) {
-    state.plausibleStats = stats.plausibleStats
-  },
-
-  setSponsorStats(state, stats) {
-    let supporterEarnings = 0
-
-    stats.supporters.forEach((supporter) => {
-      supporterEarnings +=
-        Number(supporter.support_coffees) *
-        Number(supporter.support_coffee_price)
-    })
-
-    stats.subscriptions.forEach((subscription) => {
-      supporterEarnings += Number(subscription.subscription_coffee_price)
-    })
-
-    state.sponsorStats = {
-      supporters: stats.supporters,
-      subscriptions: stats.subscriptions,
-      sponsorCount: stats.supporters.length + stats.subscriptions.length,
-      supporterEarnings,
-      subscriberCount: stats.subscriptions.length,
-      supporterCount: stats.supporters.length,
-    }
-  },
-
   setSearchResults(state, results) {
     state.searchResults.facets = results.facets.tags
     state.searchResults.results = results.nbHits
@@ -135,22 +44,6 @@ export const mutations = {
 }
 
 export const getters = {
-  youTubeStats(state) {
-    return state.youTubeStats
-  },
-
-  twitterStats(state) {
-    return state.twitterStats
-  },
-
-  plausibleStats(state) {
-    return state.plausibleStats
-  },
-
-  sponsorStats(state) {
-    return state.sponsorStats
-  },
-
   searchHits(state) {
     return state.searchResults.hits
   },
@@ -196,44 +89,6 @@ export const getters = {
 }
 
 export const actions = {
-  getYouTubeStats({ commit }) {
-    fetch(`${this.$config.base_url}api/youtube-stats`, {
-      cache: 'no-cache',
-    })
-      .then((response) => response.json())
-      .then((stats) => commit('setYouTubeStats', stats))
-  },
-
-  getTwitterStats({ commit }) {
-    fetch(`${this.$config.base_url}api/twitter-stats`, {
-      cache: 'no-cache',
-    })
-      .then((response) => response.json())
-      .then((stats) => {
-        commit('setTwitterStats', stats)
-      })
-  },
-
-  getSponsorStats({ commit }) {
-    fetch(`${this.$config.base_url}api/sponsor-stats`, {
-      cache: 'no-cache',
-    })
-      .then((response) => response.json())
-      .then((stats) => {
-        commit('setSponsorStats', stats)
-      })
-  },
-
-  getPlausibleStats({ commit }) {
-    fetch(`${this.$config.base_url}api/plausible-stats`, {
-      cache: 'no-cache',
-    })
-      .then((response) => response.json())
-      .then((stats) => {
-        commit('setPlausibleStats', stats)
-      })
-  },
-
   searchAlgolia({ commit }, payload = '') {
     commit('setSearchLoading', true)
 
